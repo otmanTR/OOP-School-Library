39fb4e8f598c1bd24e4f1fed83f43abf8918f05a
otmanTR <r.brk.otman@gmail.com>
2023-04-21 11:28:26 +0300

    create rental

--- app.rb
+++ app.rb
@@ -56,6 +56,31 @@ class App
                 puts 'Person created successfully'
             end
 
-      
+      def teacher_create
+        puts 'Age:'
+        age=gets.chomp
+        name = gets.chomp
+        puts 'Specialization:'
+        specialization = gets.chomp
+        parent_permission = true
+        @persons << Teacher.new(age: age, name: name, specialization: specialization, parent_permission: parent_permission)
+        puts 'Person created successfully'
+      end
+    
+      def new_rental
+        puts 'Select a person from the following list by numner (not id)'
+        people_list
+        person_index = gets.chomp.to_i
+
+        puts 'select a book from the following list by number'
+        book_list
+        book_index = gets.chomp.to_i
+
+        puts 'Date: '
+        date = gets.chomp
+
+        @rentals << Rental.new(date, @books[book_index], @people[person_index])
+        puts 'Rental created successfullu!'
+      end
 
 end 
